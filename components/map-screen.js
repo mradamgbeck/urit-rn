c68c75dc50da8bb1b68a39b556c61d33ed7f1c53
mradamgbeck <mradamgbeck@gmail.com>
2019-10-28 17:01:36 -0400

    MapScreen now zooms to current location on componentDidMount

--- components/map-screen.js
+++ components/map-screen.js
@@ -3,14 +3,46 @@ import { View } from 'react-native'
 import MapView from 'react-native-maps'
 import Styles from '../styles/styles'
 
+const LATITUDE = 42.2080042;
+const LONGITUDE = -83.5959087;
+const LATITUDE_DELTA = 0.009;
+const LONGITUDE_DELTA = 0.009;
+
 export default class MapScreen extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         latitude: LATITUDE,
+         longitude: LONGITUDE,
+         error: null
+      };
+   }
+   componentDidMount() {
+      navigator.geolocation.getCurrentPosition(
+         position => {
+            this.setState({
+               latitude: position.coords.latitude,
+               longitude: position.coords.longitude,
+               error: null
+            });
+         },
+         error => this.setState({ error: error.message }),
+         { enableHighAccuracy: true, timeout: 200000, maximumAge: 1000 }
+      );
+   }
    render() {
       return (
-            <View>
-               <MapView 
-               showsUserLocation={true} 
-               style={Styles.map}/>
-            </View>
+         <View>
+            <MapView
+               region={{
+                  latitude: this.state.latitude,
+                  longitude: this.state.longitude,
+                  latitudeDelta: LATITUDE_DELTA,
+                  longitudeDelta: LONGITUDE_DELTA
+               }}
+               showsUserLocation={true}
+               style={Styles.map} />
+         </View>
       );
    }
 }
